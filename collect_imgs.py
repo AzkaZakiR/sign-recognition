e523fd81956a2dd28469e14dc3d910bcb507fa6a
Azka Zaki <azkazaki11@gmail.com>
2023-08-09 23:50:39 +0700

    Fix bug and Add model

--- collect_imgs.py
+++ collect_imgs.py
@@ -2,13 +2,12 @@ import os
 
 import cv2
 
-
 DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-number_of_classes = 3
-dataset_size = 100
+number_of_classes = 10
+dataset_size = 150
 
 cap = cv2.VideoCapture(0)
 for j in range(number_of_classes):
--- recognition.py
+++ recognition.py
@@ -1,7 +1,7 @@
-import  cv2
-import  mediapipe as mp
 import pickle
 
+import cv2
+import mediapipe as mp
 import numpy as np
 
 model_dict = pickle.load(open('./model.p', 'rb'))
@@ -15,14 +15,19 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-labels_dict = {0: 'A', 1: 'B', 2: 'L'}
+#labels_dict = {0: '1', 1: '2', 2: '3', 3: '4', 4: '5', 5: '6', 6: '7', 7: '8', 8: '9', 9:'0'}
+labels_dict = { 0: '1', 1: '2', 2: '3', 3: '4', 4: '5', 5: '6', 6: '7', 7: '8', 8: '9', 9:'0', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H', 18: 'I', 19: 'L'}
+
 while True:
-    ret, frame = cap.read()
+
     data_aux = []
     x_ = []
     y_ = []
 
+    ret, frame = cap.read()
+
     H, W, _ = frame.shape
+
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     results = hands.process(frame_rgb)
@@ -38,30 +43,34 @@ while True:
         for hand_landmarks in results.multi_hand_landmarks:
             for i in range(len(hand_landmarks.landmark)):
                 x = hand_landmarks.landmark[i].x
-                y = hand_landmarks .landmark[i].y
-                data_aux.append(x)
-                data_aux.append(y)
+                y = hand_landmarks.landmark[i].y
+
                 x_.append(x)
                 y_.append(y)
 
-        x1 = int(min(x_) * W) -10
-        y1 = int(min(y_) * H) -10
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x - min(x_))
+                data_aux.append(y - min(y_))
 
-        x2 = int(max(x_) * W) -10
-        y2 = int(max(y_) * H) -10
+        x1 = int(min(x_) * W) - 10
+        y1 = int(min(y_) * H) - 10
+
+        x2 = int(max(x_) * W) - 10
+        y2 = int(max(y_) * H) - 10
 
         prediction = model.predict([np.asarray(data_aux)])
 
-        predicted = labels_dict[int(prediction[0])]
+        predicted_character = labels_dict[int(prediction[0])]
 
-        print(predicted)
-
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0,0,0), 4 )
-        cv2.putText(frame, predicted,  (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0,  0, 0), 3,
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
                     cv2.LINE_AA)
 
-    cv2.imshow('camera on', frame)
-    cv2.waitKey(5)
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
+
 
 cap.release()
-cv2.destroyAllwindows()
+cv2.destroyAllWindows()
